acc677c8c494f7b460f4f37562e5f72a2a8ea85e
KabiPriya <kabipriya.t@gmail.com>
2023-11-02 23:22:10 +0530

    added movieschema

--- app/routes/movie.routes.js
+++ app/routes/movie.routes.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-const { validate } = require("../middlewares/validate.middleware");
-const { movieSchema } = require("../validations/movie.schema");
+// const { validate } = require("../middlewares/validate.middleware");
+// const { movieSchema } = require("../validations/movie.schema");
 
-const {
-  getAllMoviesController,
-  addMovieController,
-  updateMovieController,
-  deleteMovieController,
-} = require("../controllers/movie.controller");
+// const {
+//   getAllMoviesController,
+//   addMovieController,
+//   updateMovieController,
+//   deleteMovieController,
+// } = require("../controllers/movie.controller");
 
-// READ
-router.get("/", getAllMoviesController);
+// // READ
+// router.get("/", getAllMoviesController);
 
-// CREATE
-router.post("/", validate(movieSchema), addMovieController);
+// // CREATE
+// router.post("/", validate(movieSchema), addMovieController);
 
-// UPDATE
-router.put("/:id", validate(movieSchema), updateMovieController);
+// // UPDATE
+// router.put("/:id", validate(movieSchema), updateMovieController);
 
-// DELETE
-router.delete("/:id", deleteMovieController);
+// // DELETE
+// router.delete("/:id", deleteMovieController);
 
-module.exports = router;
+// module.exports = router;
